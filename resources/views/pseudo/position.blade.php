7372bdbcc0e9161f1b995f1121ae8a25c8e221a6
Yuta Miyasaka <miyaska_y@cyberowl.co.jp>
2021-01-25 17:42:57 +0900

    mixin & media-query

--- resources/views/pseudo/position.blade.php
+++ resources/views/pseudo/position.blade.php
@@ -1,11 +1,16 @@
+<head>
 <link rel="stylesheet" href=" {{asset('css/app.css')}} ">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 
-<div class="elem1 bg-red">
-  <p>１番目の要素</p>
-  <div class="elem2 bg-blue" >
-    <p>２番目の要素</p>
-    <div class="elem3 bg-green">
-      <p>３番目の要素</p>
-    </div>
+<div>
+  <div class="elem1 media">
+    <p>メディアクエリを使った画面サイズ毎のcssの適用</p>
+    <!-- <div class="elem2 bg-blue" >
+      <p>２番目の要素</p>
+      <div class="elem3 bg-green">
+        <p>３番目の要素</p>
+      </div>
+  </div> -->
   </div>
 </div>
